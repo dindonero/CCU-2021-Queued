2310f245870d497e5fd227f5b05489896be62304
It Hurts When IP <dinis.palha@gmail.com>
2020-11-16 18:09:48 +0000

    db started

---
+++ db/createDB.bat
@@ -0,0 +1,15 @@
+SET DBNAME=ccu_db
+SET USERNAME=ccu
+SET USERPASSWORD=ccusuicidesquad
+
+psql -h localhost -p 5432 -d postgres -U postgres -c "DROP DATABASE IF EXISTS \"%DBNAME%\";"
+psql -h localhost -p 5432 -d postgres -U postgres -c "DROP OWNED BY \"%USERNAME%\" CASCADE;"
+psql -h localhost -p 5432 -d postgres -U postgres -c "DROP USER IF EXISTS \"%USERNAME%\";"
+psql -h localhost -p 5432 -d postgres -U postgres -c "CREATE DATABASE \"%DBNAME%\";"
+psql -h localhost -p 5432 -d %DBNAME% -U postgres -c "CREATE USER \"%USERNAME%\" WITH PASSWORD '%USERPASSWORD%';"
+psql -h localhost -p 5432 -d %DBNAME% -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE \"%DBNAME%\" TO \"%USERNAME%\";"
+
+psql -h localhost -p 5432 -d %DBNAME% -U %USERNAME% -f createTables.sql
+psql -h localhost -p 5432 -d %DBNAME% -U %USERNAME% -f baseInserts.sql
+
+pause
