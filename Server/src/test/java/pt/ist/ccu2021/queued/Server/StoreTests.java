b1462f1923a2e5dc189d21321fce5bbf766414de
It Hurts When IP <dinis.palha@gmail.com>
2020-11-18 19:17:22 +0000

    fixed

--- Server/src/test/java/pt/ist/ccu2021/queued/Server/StoreTests.java
+++ Server/src/test/java/pt/ist/ccu2021/queued/Server/StoreTests.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.client.RestTemplate;
 import pt.ist.ccu2021.queued.Server.dto.CompanyAccountDto;
 import pt.ist.ccu2021.queued.Server.dto.ScheduleDto;
@@ -12,6 +14,7 @@ import pt.ist.ccu2021.queued.Server.dto.StoreDto;
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootTest
 public class StoreTests {
@@ -21,7 +24,7 @@ public class StoreTests {
             .address("Rua Manuel Vaccines 31").name(ServerApplicationTests.generateRandomString()).build();
     private RestTemplate rest = new RestTemplate();
     private String url = "http://localhost:8080/company/1/store/register";
-    private String url2 = "/category/1/store";
+    private String url2 = "http://localhost:8080/category/1/store";
     private CompanyAccountDto company = CompanyAccountDto.builder().email(ServerApplicationTests.generateRandomString()).name(ServerApplicationTests.generateRandomString()).password("asdasdasd").build();
 
 
@@ -38,8 +41,13 @@ public class StoreTests {
 
     @Test
     public void whenReadingStore_ReturnsSameAsInserted(){
-        int id = rest.postForEntity(url, storeDto, Integer.class).getBody();
+        int id = rest.postForObject(url, storeDto, Integer.class);
         storeDto.setId(id);
-        Assertions.assertEquals(storeDto, rest.postForEntity());
+        // Asserts if local store is equal to the server store with the same id
+        Assertions.assertEquals(storeDto, rest.exchange(url2,
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<StoreDto>>(){}).getBody()
+                .stream().filter(x -> x.getId() == id).collect(Collectors.toList()).get(0));
     }
 }
