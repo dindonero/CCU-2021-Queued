98da55c528a1e6c609fa34a815f47d03a68e8f42
ineslacerda <ines.lacerda98@gmail.com>
2020-11-21 16:24:55 +0000

    update login screen

--- Client/lib/app_screens/signup/login.dart
+++ Client/lib/app_screens/signup/login.dart
@@ -1,4 +1,5 @@
 import 'package:ccu_app/app_screens/signup/signup.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -14,87 +15,33 @@ class _LoginScreen extends State<LoginScreen> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-        backgroundColor: const Color(0xff22bec8),
-        body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        body: Container(
+          decoration: BoxDecoration(
+          image: DecorationImage(
+          image: AssetImage("images/login_background.png"),
+          fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(children: [
+          Padding(padding: EdgeInsets.only(top: 80)),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text('Login',
-                  textAlign: TextAlign.left,
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontFamily: 'Rubik',
-                      fontSize: 40,
-                      fontWeight: FontWeight.normal,
-                      height: 1))
+              new Image.asset('images/logo_branco.png', width: 120),
+              Text('UEUED')
             ],
           ),
-          Padding(padding: EdgeInsets.only(top: 100)),
-          Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-              Text("wait ",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontFamily: 'Rubik',
-                      fontSize: 40,
-                      fontWeight: FontWeight.normal,
-                      height: 1)),
-              Text("online,",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontFamily: 'Rubik',
-                      fontSize: 40,
-                      fontWeight: FontWeight.bold,
-                      height: 1)),
-            ]),
-            Padding(padding: EdgeInsets.only(top: 10)),
-            Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-              Text(" not ",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontFamily: 'Rubik',
-                      fontSize: 40,
-                      fontWeight: FontWeight.normal,
-                      height: 1)),
-              Text("inline.",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontFamily: 'Rubik',
-                      fontSize: 40,
-                      fontWeight: FontWeight.bold,
-                      height: 1)),
-            ]),
-            Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-              Text(" Click here to sign up -> ",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontFamily: 'Rubik',
-                      fontSize: 15,
-                      fontWeight: FontWeight.normal,
-                      height: 1)),
-              Container(
-                  padding: EdgeInsets.all(0),
-                  child: IconButton(
-                    padding: EdgeInsets.all(0),
-                    alignment: Alignment.centerRight,
-                    icon: (Icon(Icons.star)),
-                    color: Colors.red[500],
-                    onPressed: signup,
-                  )),
-            ]),
-            Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  InkWell(
-                    onTap: signup,
-                    child: Padding(
-                      padding: EdgeInsets.all(10.0),
-                      child: Text("Haven't signed up yet? Click here"),
-                    ),
-                  ),
-                ])
-          ])
-        ]));
+          Padding(padding: EdgeInsets.only(top: 150)),
+          Text("Welcome", style: TextStyle(color: Color(0xff13497b), fontWeight: FontWeight.bold)),
+          Text("Login to start waiting online.", style: TextStyle(color: Color(0x50000000), fontSize: 25)),
+          InkWell(
+            onTap: signup,
+            child: Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Text("Haven't signed up yet? Click here", style: TextStyle(color: Color(0x50000000))),
+            ),
+          ),
+        ])));
   }
 
   void signup() {
