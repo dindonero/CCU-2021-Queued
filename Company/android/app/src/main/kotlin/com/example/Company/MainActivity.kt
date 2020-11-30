7a081af03208e4aba5e3188f7e715b343a0d9a4b
It Hurts When IP <dinis.palha@gmail.com>
2020-11-30 17:48:18 +0000

    cleaning repository

--- Company/android/app/src/main/kotlin/com/example/Company/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.Company
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Company/android/app/src/main/kotlin/com/example/Queued/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.Queued
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
