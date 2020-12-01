878ee6221185a5982132fc2db81515bbd0e6bfa5
ItHurtsWhenIP <dinis.palha@gmail.com>
2020-12-01 18:28:17 +0000

    amariaequeimadissimavemaiumconlitodosgrandes

--- Queued/lib/app_screens/category.dart
+++ Queued/lib/app_screens/category.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Category {
-  final String image;
+  final Image image;
   final String title;
   final int id;
   final Color color;
@@ -13,37 +13,3 @@ class Category {
     this.color,
   });
 }
-
-List<Category> categories = [
-  Category(
-      id: 1,
-      title: "Supermarket",
-      image: "images/supermarkets.png",
-      color: Color(0xFFFFFFFF)),
-  Category(
-      id: 2,
-      title: "Pharmacy",
-      image: "images/phar.png",
-      color: Color(0xFFFFFFFF)),
-  Category(
-      id: 3,
-      title: "Coffee Shop",
-      image: "images/coffeeShop.png",
-      color: Color(0xFFFFFFFF)),
-  Category(
-      id: 4,
-      title: "Coffee",
-      image: "images/coffee.png",
-      color: Color(0xFFFFFFFF)),
-  Category(
-      id: 5,
-      title: "Hospital",
-      image: "images/hospital.png",
-      color: Color(0xFFFFFFFF)),
-  Category(
-      id: 6,
-      title: "Restaurant",
-      image: "images/restaurant.png",
-      color: Color(0xFFFFFFFF)),
-];
-
