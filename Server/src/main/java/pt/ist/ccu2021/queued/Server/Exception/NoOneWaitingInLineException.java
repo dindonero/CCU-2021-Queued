960df3ec2a5efef35e3f9d2b5f07b04ced07adbd
It Hurts When IP <dinis.palha@gmail.com>
2020-12-04 23:57:16 +0000

    "Pq sem esforço nao vao a lado nenhum"

--- Server/src/main/java/pt/ist/ccu2021/queued/Server/Exception/NoOneWaitingInLineException.java
+++
@@ -1,8 +0,0 @@
-package pt.ist.ccu2021.queued.Server.Exception;
-
-public class NoOneWaitingInLineException extends Exception{
-
-    public NoOneWaitingInLineException(int id, String name) {
-        super("No one is waiting in line for the given counter - CounterId:" + id + ", CounterName:" + name);
-    }
-}
