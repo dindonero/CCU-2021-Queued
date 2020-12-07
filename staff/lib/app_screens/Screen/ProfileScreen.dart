5a8e4dea31d5add9c392c203e574b8e7d8a5b784
madDuarte <45204585+madDuarte@users.noreply.github.com>
2020-12-07 00:46:38 +0000

    Added Profile Screen in Staff

---
+++ staff/lib/app_screens/Screen/ProfileScreen.dart
@@ -0,0 +1,91 @@
+import 'dart:math' as math;
+import 'package:Staff/services/ServerCommunicationService.dart';
+import 'package:Staff/app_screens/Screen/AllStaffStoresScreen.dart';
+import 'package:Staff/app_screens/Screen/StoreOnScreen.dart';
+import 'package:Staff/app_screens/Widget/navBarWidget.dart';
+import 'package:flutter/material.dart';
+
+
+class ProfileScreen extends StatefulWidget {
+   ProfileScreen();
+
+  @override
+  _ProfileScreenState createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {  
+
+  _ProfileScreenState();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xffF8FBFF),
+      body: Column(children: <Widget>[
+        Expanded(
+          child: Column( 
+            children: <Widget>[
+              SizedBox(height: screenSize().height / 15),
+              mainRow(),
+              Container( width: 350, child: Divider(thickness: 2, color: Color(0xFF676666),)),
+              SizedBox(height: screenSize().height / 30),
+              Container( alignment: Alignment.center,
+                width: 350,
+                height: 60,
+                decoration: BoxDecoration(
+                                          border: Border.all(color: Color(0xFF13497B),),
+                                          color:  Color(0xFF13497B),
+                                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                                           boxShadow: [
+                                                        BoxShadow(
+                                                          color: Colors.grey.withOpacity(0.5),
+                                                          spreadRadius: 5,
+                                                          blurRadius: 7,
+                                                          offset: Offset(0, 3), // changes position of shadow
+                                                        ),
+                                                      ],
+                                        ),
+                child: Padding(padding:  EdgeInsets.symmetric(horizontal: screenSize().width / 5),
+                        child: Row(children: [ 
+                              Icon(Icons.notifications_outlined, color: Colors.white, size: 30,),
+                              Text('Notifications', style: TextStyle(color: Colors.white, fontSize: 20,fontWeight: FontWeight.bold,),),
+               
+                        ]),
+
+                       ),
+               ),
+            
+            ],
+          ),
+        ),
+        Nav(1),
+      ]),
+    );
+  }
+
+  
+
+  Size screenSize() {
+    return MediaQuery.of(context).size;
+  }
+
+  Row mainRow() {
+    return Row(
+     // mainAxisAlignment: MainAxisAlignment.start,
+      children: [   
+        Padding(padding:  EdgeInsets.only(left: screenSize().width / 25),
+              child:  Padding(padding: EdgeInsets.only(right: screenSize().width / 80),
+                      child:  Icon(Icons.account_circle_rounded,color: Color(0xFFBFBFBF),size: 80, ),
+              ),
+        ),
+        Align(alignment: Alignment.centerLeft,
+              child: Column(mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                      Text('Staff               ',style: TextStyle(color: Color(0xFF000000),fontSize: 22, fontWeight: FontWeight.bold,),),
+                      Text('View Account', style: TextStyle(color: Color(0xFF22BEC8),fontSize: 20,fontWeight: FontWeight.bold,),),
+                ],),
+         ),],
+     );
+   }
+ 
+}
